6871f5a7bd8dc0b2e32dce8f6d8c149ac057c586
F1F7Y <filip.bartos07@proton.me>
2026-01-30 22:21:03 +0100

    make script beterererer

--- scripts/map/infection_point.gd
+++ scripts/map/infection_point.gd
@@ -1,26 +1,27 @@
 extends Node2D
 
 @export var time_to_infect_sec: float = 5
+@export var progress_loss_sec: float = 2
 var is_player_inside: bool = false
+
+@onready var progress_bar: ProgressBar = $ProgressBar
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$ProgressBar.hide()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if (is_player_inside):
-		$ProgressBar.value += (100 * delta) / time_to_infect_sec
+		progress_bar.value += (100 * delta) / time_to_infect_sec
+	else:
+		progress_bar.value -= (100 * delta) / progress_loss_sec
 	pass
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if (body.name == "Player"):
 		is_player_inside = true
-		$ProgressBar.value = 0
-		$ProgressBar.show()
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if (body.name == "Player"):
 		is_player_inside = false
-		$ProgressBar.hide()
