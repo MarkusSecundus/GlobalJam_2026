eaadc5397619cf9c513f5aa08188598440e9b791
F1F7Y <filip.bartos07@proton.me>
2026-01-31 09:49:31 +0100

    infection point infecting + ui

--- scripts/map/infection_point.gd
+++ scripts/map/infection_point.gd
@@ -2,21 +2,34 @@ extends Node2D
 
 @export var time_to_infect_sec: float = 5
 @export var progress_loss_sec: float = 2
+
 var is_player_inside: bool = false
+var is_infected: bool = false
 
 @onready var progress_bar: ProgressBar = $ProgressBar
+@onready var infection_sprite: Sprite2D = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	InfectionSingleton.infected_point_count += 1
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if (is_infected):
+		return
+	
 	if (is_player_inside):
 		progress_bar.value += (100 * delta) / time_to_infect_sec
 	else:
 		progress_bar.value -= (100 * delta) / progress_loss_sec
-	pass
+		
+	if progress_bar.value >= 100 and not is_infected:
+		is_infected = true
+		InfectionSingleton.infected_points += 1
+		progress_bar.hide()
+		infection_sprite.scale.y *= -1
+		# Change sprite here????
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if (body.name == "Player"):
