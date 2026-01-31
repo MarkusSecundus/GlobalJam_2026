77a56e502609236245dd2da09a2900a0a337c31d
MarkusSecundus <kubahronik.g@gmail.com>
2026-01-31 13:50:09 +0100

    fix scenes being broken

--- scripts/util/dialog_system/simple_actions/DialogActionTransitionScene.gd
+++ scripts/util/dialog_system/simple_actions/DialogActionTransitionScene.gd
@@ -1,6 +1,9 @@
 extends IDialogAction
 
 @export var destination : PackedScene
+@export var destination_string : String
 
 func do_perform(_ctx: DialogContext, on_finished: Callable)->void:
-	get_tree().change_scene_to_packed(destination)
+	if destination: get_tree().change_scene_to_packed(destination)
+	else: get_tree().change_scene_to_file(destination_string)
+	
