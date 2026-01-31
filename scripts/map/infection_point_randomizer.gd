f6e1eb9c030ccf63be14a94fb2b22fe718169162
MarkusSecundus <kubahronik.g@gmail.com>
2026-01-31 19:50:26 +0100

    fix infection point randomizer bug

--- scripts/map/infection_point_randomizer.gd
+++ scripts/map/infection_point_randomizer.gd
@@ -9,5 +9,5 @@ func do_randomize()->void:
 	var total_infection_points :Array= NodeUtils.get_descendants_of_type(get_tree().root, InfectionPoint)
 	while total_infection_points.size() > total_infection_points_count:
 		var idx := randi_range(0, total_infection_points.size() - 1)	
-		(total_infection_points[idx] as InfectionPoint).queue_free()
+		(total_infection_points[idx] as InfectionPoint).free()
 		total_infection_points.remove_at(idx)
