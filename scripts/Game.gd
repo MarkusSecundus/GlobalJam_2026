ba49ebb2579c2ca41387f271345705f83ea20466
raivis <raivisberzins000@gmail.com>
2026-01-31 10:15:42 +0100

    GameState.gd

---
+++ scripts/Game.gd
@@ -0,0 +1,7 @@
+extends Node
+
+enum Mask {
+	TRI    = 0b0001,
+	RECT   = 0b0010,
+	CIRCLE = 0b0100,
+}
--- scripts/GameState.gd
+++ scripts/GameState.gd
@@ -1,3 +1,5 @@
 extends Node
 
 var player_hp: int = 3
+
+var player_shape: int = 0  # bitmask of Game.Mask values (see Game.gd)
