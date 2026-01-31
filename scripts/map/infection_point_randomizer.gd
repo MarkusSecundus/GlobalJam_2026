b324e57d5d82b7bd38f81e67df162bcc98585c57
MarkusSecundus <kubahronik.g@gmail.com>
2026-01-31 12:30:18 +0100

    fix dumb bug

--- scripts/map/infection_point_randomizer.gd
+++ scripts/map/infection_point_randomizer.gd
@@ -8,6 +8,6 @@ func _ready() -> void:
 func do_randomize()->void:
 	var total_infection_points :Array= NodeUtils.get_descendants_of_type(get_tree().root, InfectionPoint)
 	while total_infection_points.size() > total_infection_points_count:
-		var idx := randi_range(0, total_infection_points.size())	
+		var idx := randi_range(0, total_infection_points.size() - 1)	
 		(total_infection_points[idx] as InfectionPoint).queue_free()
 		total_infection_points.remove_at(idx)
