6ab5345c6bee96b768554f2875ca6e9a2d263d5c
raivis <raivisberzins000@gmail.com>
2026-01-31 08:39:44 +0100

    enemy update

--- prefabs/enemy.gd
+++ prefabs/enemy.gd
@@ -3,6 +3,9 @@ extends RigidBody2D
 # if nonzero = moving towards the player
 var attack_dir: Vector2 = Vector2.ZERO
 
+# 0 to 1.0 - full aggro
+var aggro_level: float = 0.0
+var detected_player: Node2D = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,19 +13,35 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	if attack_dir.length_squared() > 0:
-		apply_central_impulse(500.0 * attack_dir)
+		apply_central_impulse(-self.linear_velocity) # kill any velocity we had
+		apply_central_impulse(1000.0 * attack_dir)
 		attack_dir = Vector2.ZERO
 	pass
 
 func _draw() -> void:
+	var col = lerp(Color.WHITE, Color.DEEP_PINK, aggro_level)
 	draw_arc($PlayerDamageArea.position,
 			$PlayerDetectArea/CollisionShape2D.shape.radius,
-			0, TAU, 32, Color.WHITE)
+			0, TAU, 32, col)
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var initial_aggro_level = aggro_level
+	if detected_player:
+		aggro_level += 1.0 * delta
+	else:
+		aggro_level -= 0.5 * delta
+	aggro_level = clampf(aggro_level, 0.0, 1.0)
+	
+	if aggro_level != initial_aggro_level:
+		queue_redraw()
+	
+	# detected_player should always be there, but just in case
+	if detected_player and aggro_level >= 1.0:
+		attack_dir = (detected_player.global_position - self.position).normalized()
+		aggro_level = 0.0
+		
 
 
 func _on_player_damage_area_body_entered(body: Node2D) -> void:
@@ -30,6 +49,12 @@ func _on_player_damage_area_body_entered(body: Node2D) -> void:
 
 
 func _on_player_detect_area_body_entered(body: Node2D) -> void:
-	if NodeUtils.get_ancestor_of_type(body, Player):
-		attack_dir = (body.global_position - self.position).normalized()
-	pass # Replace with function body.
+	# do I need to use Jakub's helper?
+	# if NodeUtils.get_ancestor_of_type(body, Player):
+	if body is Player:
+		detected_player = body
+
+
+func _on_player_detect_area_body_exited(body: Node2D) -> void:
+	if body is Player:
+		detected_player = null
