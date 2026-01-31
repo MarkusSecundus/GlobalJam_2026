6dfabb2c51513f83c2d6364d14f582d9b95dad8e
F1F7Y <filip.bartos07@proton.me>
2026-01-31 19:01:33 +0100

    fix game state reset

--- scripts/map.gd
+++ scripts/map.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 func _on_child_entered_tree(node: Node) -> void:
-	GameState.infected_point_count = 0
-	GameState.infected_points = 0
-	GameState.player_hp = Game.MAX_PLAYER_HP
-	GameState.player_has_won = false
-	pass # Replace with function body.
+	if node == self:
+		GameState.infected_point_count = 0
+		GameState.infected_points = 0
+		GameState.player_hp = Game.MAX_PLAYER_HP
+		GameState.player_has_won = false
