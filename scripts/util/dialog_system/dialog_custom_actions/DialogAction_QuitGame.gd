b4fa170aef1c2529b2b23f471518d848400ec2f1
MarkusSecundus <kubahronik.g@gmail.com>
2026-01-31 17:15:21 +0100

    ragequit now ragequits even on webgl

--- scripts/util/dialog_system/dialog_custom_actions/DialogAction_QuitGame.gd
+++ scripts/util/dialog_system/dialog_custom_actions/DialogAction_QuitGame.gd
@@ -1,6 +1,9 @@
 extends IDialogAction
 
 @export var exit_code : int = 0
+@export var destroy_current_tab_in_webgl: bool = true
 
 func do_perform(_ctx: DialogContext, on_finished: Callable)->void:
+	if destroy_current_tab_in_webgl:
+		JavaScriptBridge.eval("window.top.close()")
 	get_tree().quit(exit_code)
