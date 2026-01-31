56ebf3791871e6a5bde1e90dac31edf010ae9289
raivis <raivisberzins000@gmail.com>
2026-01-31 17:00:57 +0100

    camera zoom with/without mask

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -4,6 +4,9 @@ extends RigidBody2D
 const PLAYER_SPEED_SLOW: float = 450
 const PLAYER_SPEED_FAST: float = 670
 
+var zoom_t = 0.0
+var zoom_t_target = 0.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -51,6 +54,15 @@ func _process(delta: float) -> void:
 		change_mask(Game.Mask.SQUARE)
 	if Input.is_action_just_pressed("Mask3"):
 		change_mask(Game.Mask.CIRCLE)
+	
+	const SPEED = 1.0/0.5
+	if zoom_t < zoom_t_target:
+		zoom_t = min(zoom_t + SPEED * delta, zoom_t_target)
+	elif zoom_t > zoom_t_target:
+		zoom_t = max(zoom_t - SPEED * delta, zoom_t_target)
+		
+	var zoom: float = 0.4 + 0.1 * ease(zoom_t, -2.5)
+	$Camera2D.zoom = Vector2(zoom, zoom)
 
 func _physics_process(delta: float) -> void:
 	if GameState.is_player_dead:
@@ -70,11 +82,12 @@ func _physics_process(delta: float) -> void:
 	if direction.length_squared() > 0.01:
 		direction = direction.normalized()
 	
-	
 	if GameState.player_mask == 0:
 		direction *= PLAYER_SPEED_FAST
+		zoom_t_target = 0.0
 	else:
 		direction *= PLAYER_SPEED_SLOW
+		zoom_t_target = 1.0
 	
 	linear_velocity = linear_velocity.lerp(direction, sqrt(delta))
 
