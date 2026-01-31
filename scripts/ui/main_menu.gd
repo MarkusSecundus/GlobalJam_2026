38e4fe6910229f78f51765c4ebeccfff3b00a64f
F1F7Y <filip.bartos07@proton.me>
2026-01-31 13:58:29 +0100

    reset values when starting play

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,11 +1,7 @@
 extends Control
 
 
-func _on_button_play_button_up() -> void:
-	get_tree().change_scene_to_file("res://maps/test_map.tscn")
-	pass # Replace with function body.
-
-
-func _on_button_quit_button_up() -> void:
-	get_tree().quit()
-	pass # Replace with function body.
+func _on_button_play_pressed() -> void:
+	GameState.infected_point_count = 0
+	GameState.infected_points = 0
+	GameState.player_hp = Game.MAX_PLAYER_HP
