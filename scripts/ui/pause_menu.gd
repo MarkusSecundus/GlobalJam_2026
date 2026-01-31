44f505f58f22a20c3f06c09e3b3a19e99891b60d
F1F7Y <filip.bartos07@proton.me>
2026-01-31 13:35:20 +0100

    broken pause menu

---
+++ scripts/ui/pause_menu.gd
@@ -0,0 +1,29 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	visible = false
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Pause"):
+		if get_tree().paused == true:
+			visible = false
+			get_tree().paused = false
+		else:
+			visible = true
+			get_tree().paused = true
+
+
+func _on_button_resume_pressed() -> void:
+	get_tree().paused = false
+	visible = false
+
+
+func _on_button_return_pressed() -> void:
+	get_tree().paused = false
+	#visible = false
+	#get_tree().change_scene_to_file("res://ui/main_menu.tscn")
