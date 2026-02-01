2705652c2d2535303c0cb6f680af1e6c9dbf37c3
F1F7Y <filip.bartos07@proton.me>
2026-02-01 11:28:54 +0100

    new hearts

--- scripts/ui/healt_display.gd
+++ scripts/ui/healt_display.gd
@@ -12,6 +12,6 @@ func _process(delta: float) -> void:
 	for ii in range(Game.MAX_PLAYER_HP):
 		var heart :TextureRect = self.get_child(ii) as TextureRect
 		if ii < GameState.player_hp:
-			heart.texture = preload("res://textures/heart_full.png")
+			heart.texture = preload("res://textures/life.webp")
 		else:
-			heart.texture = preload("res://textures/heart_broken.png")
+			heart.texture = preload("res://textures/no_lie.webp")
